91df3be800370544aae70a26d716aeda9f9edb8d
Caio Cezar Dias <caiocd007@gmail.com>
2025-10-03 08:56:56 -0300

    Add `ControllerAtividade` and `DatabaseHandler` for activity management and database interaction

---
+++ Controller/ControllerAtividade.py
@@ -0,0 +1,33 @@
+﻿from Model import Atividade
+from View import MenuSimples
+from Generic import Utils
+
+
+class ControllerAtividade:
+    def __init__(self, list_atividades: list):
+        self.list_atividades = list_atividades
+
+    def cadastrar_atividade(self):
+        try:
+            acao = input("\nDigite a ação da atividade: ")
+            origens = ["AcoesConcGmax", "AcoesConcSap"]
+            origem_selecionada = MenuSimples("Selecione a origem: ", origens).choose(include_exit=False)
+
+            unidades_pagamento = ["NS", "US"]
+            unidade_selecionada = MenuSimples("Seleciona a unidade de pagamento", unidades_pagamento).choose(include_exit=False)
+
+            valor_unidade = Utils.read_float("\nSelecione o valor pago por unidade: ")
+
+            atividade_criada = Atividade(acao, origens[origem_selecionada], unidades_pagamento[unidade_selecionada], valor_unidade)
+
+            self.list_atividades.append(atividade_criada)
+
+            return "Atvidade criada com sucesso."
+        except Exception as e:
+            return f"\nErro ao criar atividade. {str(e)}"
+
+if __name__ == "__main__":
+    lista = []
+    controller = ControllerAtividade(lista)
+    controller.cadastrar_atividade()
+    print(str(lista[0]))
---
+++ Data/DatabaseHandler.py
@@ -0,0 +1,22 @@
+﻿import pyodbc
+from sqlalchemy import create_engine
+
+class DatabaseHandler:
+    def __init__(self, server: str, database: str, username: str, password: str):
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+
+    def get_pyodbc_connection(self):
+        connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={self.server};DATABASE={self.database};UID={self.username};PWD={self.password};Encrypt=yes;TrustServerCertificate=yes"
+        return pyodbc.connect(connection_string)
+
+    def get_sqlalchemy_engine(self):
+        connection_url = (
+            f"mssql+pyodbc://{self.username}:{self.password}@{self.server}/{self.database}?"
+            f"driver=ODBC+Driver+18+for+SQL+Server"
+            f"&Encrypt=yes"
+            f"&TrustServerCertificate=yes"
+        )
+        return create_engine(connection_url)
