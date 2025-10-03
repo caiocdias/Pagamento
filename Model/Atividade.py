f9fefc343b1ff1d560089979ce431b9732a5145d
Caio Cezar Dias <caiocd007@gmail.com>
2025-10-03 08:56:30 -0300

    Add `Atividade` and `Pessoa` model classes to represent core entities

---
+++ Model/Atividade.py
@@ -0,0 +1,9 @@
+﻿class Atividade:
+    def __init__(self, acao: str, origem: str, unidade_pagamento: str, valor_unidade: float):
+        self.acao = acao
+        self.origem = origem
+        self.unidade_pagamento = unidade_pagamento
+        self.valor_unidade = valor_unidade
+
+    def __str__(self):
+        return f"{self.acao}, {self.origem} - {self.valor_unidade} por {self.unidade_pagamento}"
---
+++ Model/Pessoa.py
@@ -0,0 +1,9 @@
+﻿from Model import Atividade
+
+class Pessoa:
+    def __init__(self, nome: str, matricula: str, chave_pix: str, email: str):
+        self.nome = nome
+        self.matricula = matricula
+        self.chave_pix = chave_pix
+        self.email = email
+        self.atividades = list[Atividade]
