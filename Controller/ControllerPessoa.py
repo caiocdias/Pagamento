5e7d62fc9baff172c0a14899f90517a8601939bf
Caio Cezar Dias <caiocd007@gmail.com>
2025-10-06 08:15:53 -0300

    Add `ControllerPessoa` class for managing `Pessoa` entities, including CRUD operations, activity management, and data persistence

---
+++ Controller/ControllerPessoa.py
@@ -0,0 +1,148 @@
+﻿import pickle
+from math import trunc
+
+from Model import Pessoa
+from View import MenuSimples
+
+
+class ControllerPessoa:
+    def __init__(self, lista_pessoas: list, lista_atividades: list):
+        self.lista_pessoas = lista_pessoas
+        self.lista_atividades = lista_atividades
+
+    def cadastrar(self):
+        try:
+            nome = input("Digite o nome da pessoa: ")
+            matricula = input("Digite a mátricula: ")
+            chave_pix = input("Digite a chave pix: ")
+            email = input("Digite o email: ")
+
+            opcoes_atividade = [str(atv) for atv in self.lista_atividades]
+            atividades = []
+            while True:
+                opcao_selecionada = MenuSimples("Atividade para Atribuir", opcoes_atividade).choose(include_exit=True)
+
+                if opcao_selecionada is None:
+                    break
+
+                atividades.append(self.lista_atividades[opcao_selecionada])
+
+            pessoa = Pessoa(nome, matricula, chave_pix, email)
+
+            for atv in atividades:
+                pessoa.atribuir_atividade(atv)
+
+            self.lista_pessoas.append(pessoa)
+            return "Pessoa cadastrada com sucesso."
+
+        except Exception as e:
+            return f"Erro ao cadastrar pessoa. {str(e)}"
+    
+    def remover(self):
+        try:
+            if len(self.lista_pessoas) == 0:
+                raise Exception("Não há pessoas cadastradas.")
+
+            pessoa_selecionada = self.listar("Selecionae a pessoa para remover", choose_flag=True)
+
+            if pessoa_selecionada is None:
+                return "Nenhuma pessoa foi removida."
+
+            confirmar = input("Tem certeza que deseja remover a pessoa? (y): ").lower()
+            if confirmar != 'y':
+                return "Nenhuma pessoa foi removida."
+
+            self.lista_pessoas.pop(pessoa_selecionada)
+
+            return "Pessoa removida com sucesso"
+        except Exception as e:
+            return f"\nErro ao remover pessoa. {str(e)}"
+
+    def alterar(self):
+        try:
+            if len(self.lista_pessoas) == 0:
+                raise Exception("Não há pessoas cadastradas.")
+
+            pessoa_selecionada = self.listar("Selecione a pessoa para alterar", choose_flag=True)
+
+            if pessoa_selecionada is None:
+                return "Nenhuma pessoa foi alterada."
+
+            confirmar = input("Tem certeza que deseja alterar a pessoa? (y): ").lower()
+            if confirmar != 'y':
+                return "Nenhuma pessoa foi alterada."
+
+            opcoes = ["Alterar nome", "Alterar matricula", "Alterar chave pix", "Alterar email", "Alterar atividades."]
+            opcao_selecionada = MenuSimples("Selecione o atributo para alterar", opcoes).choose(include_exit=True)
+
+            match opcao_selecionada:
+                case 0:
+                    nome = input("Digite o novo nome: ")
+                    self.lista_pessoas[pessoa_selecionada].set_nome(nome)
+                case 1:
+                    matricula = input("Digite a nova mátricula: ")
+                    self.lista_pessoas[pessoa_selecionada].set_matricula(matricula)
+                case 2:
+                    chave_pix = input("Digite a nova chave pix: ")
+                    self.lista_pessoas[pessoa_selecionada].set_chave_pix(chave_pix)
+                case 3:
+                    email = input("Digite o novo email: ")
+                    self.lista_pessoas[pessoa_selecionada].set_email(email)
+                case 4:
+                    self._alterar_atividade(pessoa_selecionada)
+                case _:
+                    raise ValueError("Erro no atributo selecionado")
+            return "Pessoa alterada com sucesso."
+        except Exception as e:
+            return f"\nErro ao alterar pessoa. {str(e)}"
+
+    def listar(self, title: str, choose_flag: bool = False):
+        menu = MenuSimples(title, [str(atv) for atv in self.lista_pessoas])
+        if choose_flag:
+            return menu.choose(include_exit=True)
+
+        menu.show(include_exit=False)
+        return None
+    def salvar_lista(self):
+        path = "..\\compressed_data\\lista_pessoas.pkl"
+        with open(path, 'wb') as file:
+            pickle.dump(self.lista_pessoas, file)
+
+    def carregar_lista(self):
+        path = "..\\compressed_data\\lista_pessoas.pkl"
+        with open(path, 'rb') as file:
+            self.lista_pessoas = pickle.load(file)
+
+    def _alterar_atividade(self, pessoa_selecionada):
+        while True:
+            opcoes = ["Adicionar Atividade", "Remover Atividade"]
+            opcao_selecionada = MenuSimples("Selecione o que fazer com atividades", opcoes).choose(include_exit=True)
+
+            if opcao_selecionada is None :
+                return True
+
+            opcoes_atividade = [str(atv) for atv in self.lista_atividades]
+            atividades_existentes = [str(atv) for atv in self.lista_pessoas[pessoa_selecionada].lista_atividades]
+
+            opcoes_disponiveis = [op for op in opcoes_atividade if op not in atividades_existentes]
+            match opcao_selecionada:
+                case 0:
+                    while True:
+                        atividade_selecionada = MenuSimples("Selecione a atividade", opcoes_disponiveis).choose(include_exit=True)
+
+                        if atividade_selecionada is None:
+                            break
+
+                        if self.lista_atividades[atividade_selecionada] in self.lista_pessoas[pessoa_selecionada].lista_atividades:
+                            print("Este usuário já possui essa atividade.")
+
+                        self.lista_pessoas[pessoa_selecionada].atribuir_atividade(self.lista_atividades[atividade_selecionada])
+                case 1:
+                    while True:
+                        atividade_para_remover = MenuSimples("Selecione a atividade para remover", [str(atv) for atv in self.lista_pessoas[pessoa_selecionada].lista_atividades])
+                        if atividade_para_remover is None:
+                            break
+
+                        self.lista_pessoas[pessoa_selecionada].remover_atividade(atividade_para_remover)
+                case _:
+                    raise ValueError("Erro no atributo selecionado")
