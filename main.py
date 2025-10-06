01716f446c33610e842949532368a997aac2088f
Caio Cezar Dias <caiocd007@gmail.com>
2025-10-06 09:59:57 -0300

    Add `main.py` as the program entry point with menu logic and integration of controllers

---
+++ main.py
@@ -0,0 +1,83 @@
+﻿import sys
+from Controller import ControllerAtividade, ControllerPessoa
+from View import MenuSimples
+
+
+lista_atividades = []
+lista_pessoas = []
+
+
+controller_atividades = ControllerAtividade(lista_atividades)
+print(controller_atividades.carregar())
+
+controller_pessoas = ControllerPessoa(lista_pessoas, lista_atividades)
+print(controller_pessoas.carregar())
+
+
+while True:
+    try:
+        opcoes = ["Menu de Atividades", "Menu de Pessoas", "Salvar e Sair", "Sair sem Salvar"]
+        opcao_selecionada = MenuSimples("Menu Principal", opcoes).choose(include_exit=False)
+
+        match opcao_selecionada:
+            case 0:
+                while True:
+                    opcao_menu = controller_atividades.menu_principal()
+                    match opcao_menu:
+                        case 0:
+                            print(controller_atividades.cadastrar())
+                            input()
+                        case 1:
+                            print(controller_atividades.remover())
+                            input()
+                        case 2:
+                            print(controller_atividades.alterar())
+                            input()
+                        case None:
+                            print("Retornando ao menu principal.")
+                            input()
+                            break
+                        case _:
+                            raise ValueError("Erro no atributo selecionado")
+
+            case 1:
+                while True:
+                    opcao_menu = controller_pessoas.menu_principal()
+                    match opcao_menu:
+                        case 0:
+                            print(controller_pessoas.cadastrar())
+                            input()
+                        case 1:
+                            print(controller_pessoas.remover())
+                            input()
+                        case 2:
+                            print(controller_pessoas.alterar())
+                            input()
+                        case None:
+                            print("Retornando ao menu principal.")
+                            input()
+                            break
+                        case _:
+                            raise ValueError("Erro no atributo selecionado")
+
+            case 2:
+                retorno_salvar_atividades = controller_atividades.salvar()
+                retorno_salvar_pessoas = controller_pessoas.salvar()
+
+                print(retorno_salvar_atividades)
+                print(retorno_salvar_pessoas)
+
+                if retorno_salvar_atividades == "Arquivo de atividades salvo com sucesso." and retorno_salvar_pessoas == "Arquivo de pessoas salvo sucesso.":
+                    input("Obrigado por utilizar o programa.\nAutor: Caio Cezar Dias\nContato: caiocd007@gmail.com\n\nPressione ENTER para fechar.")
+                    sys.exit()
+
+                print("Não foi possível salvar e sair.")
+
+            case 3:
+                input("Obrigado por utilizar o programa.\nAutor: Caio Cezar Dias\nContato: caiocd007@gmail.com\n\nPressione ENTER para fechar.")
+                sys.exit()
+            case _:
+                input("A opções selecionada foi inválida")
+
+    except Exception as e:
+        print(f"Erro na iteração principal. {str(e)}")
