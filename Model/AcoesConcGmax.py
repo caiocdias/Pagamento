08dca1086dbbf1e0b38abe80faad37f9cbf22f90
Caio Cezar Dias <caiocd007@gmail.com>
2025-10-06 14:02:41 -0300

    Add `AcoesConcGmax` class for loading data based on date range from `vBIAcoes` table

---
+++ Model/AcoesConcGmax.py
@@ -0,0 +1,15 @@
+﻿import pandas as pd
+
+from Data import DatabaseHandler
+
+class AcoesConcGmax:
+    def __init__(self, start_date: str, end_date: str):
+        self.dataframe = None
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def load_dataframe(self):
+        query = f"SELECT * FROM vBIAcoes WHERE ACOES_DAT_CONCLUSAO BETWEEN cast('{self.start_date}' as datetime) AND cast('{self.end_date}' as datetime)"
+        db = DatabaseHandler('192.168.10.250', 'GMAX', 'U_PBI_READ', 'GMaxWebBI#2025')
+
+        self.dataframe = pd.read_sql(query, db.get_sqlalchemy_engine())
