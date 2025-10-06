549364f02c17d3fbc52b9c37915fd77506269321
Caio Cezar Dias <caiocd007@gmail.com>
2025-10-06 14:30:54 -0300

    Add `ControllerAcoesConcGmax` for managing `AcoesConcGmax` model with date range initialization

---
+++ Controller/ControllerAcoesConcGmax.py
@@ -0,0 +1,9 @@
+﻿from Model import AcoesConcGmax
+
+class ControllerAcoesConcGmax:
+    def __init__(self, lista_pessoas : list, start_date: str, end_date: str):
+        self.start_date = start_date
+        self.end_date = end_date
+
+        self.AcoesConcGmax = AcoesConcGmax(self.start_date, self.end_date)
+
