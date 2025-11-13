37c90ccb8b5afa35bfe3dcd948c51861b2809582
Caio Cezar Dias <caiocd007@gmail.com>
2025-11-13 14:40:17 -0300

    Refactor main loop structure and enhance exception handling
    - Encapsulate controller initialization and main loop logic within a try-except block for improved error handling.
    - Refactor nested match-case blocks for better readability and consistency.
    - Streamline repetitive code in menus for `Atividades`, `Pessoas`, and `Supervisores`.

--- main.py
+++ main.py
@@ -1,50 +1,95 @@
 ﻿import traceback
 from datetime import datetime
-from Generic import *
 from Controller import ControllerAtividade, ControllerPessoa, ControllerSupervisor
 from Controller.ControllerAcoesConcGmax import ControllerAcoesConcGmax
 import sys
 from View import MenuSimples
 
+try:
+    lista_atividades = []
+    lista_pessoas = []
+    lista_supervisores = []
 
-lista_atividades = []
-lista_pessoas = []
-lista_supervisores = []
+    controller_atividades = ControllerAtividade(lista_atividades)
+    print(controller_atividades.carregar())
+    controller_pessoas = ControllerPessoa(lista_pessoas, controller_atividades.lista_atividades)
+    print(controller_pessoas.carregar())
 
-controller_atividades = ControllerAtividade(lista_atividades)
-print(controller_atividades.carregar())
-controller_pessoas = ControllerPessoa(lista_pessoas, controller_atividades.lista_atividades)
-print(controller_pessoas.carregar())
+    controller_pessoas.reconciliar_atividades()
 
-controller_pessoas.reconciliar_atividades()
+    controller_supervisores = ControllerSupervisor(lista_supervisores, controller_pessoas.lista_pessoas)
+    print(controller_supervisores.carregar())
 
-controller_supervisores = ControllerSupervisor(lista_supervisores, controller_pessoas.lista_pessoas)
-print(controller_supervisores.carregar())
+    controller_supervisores.reconciliar_pessoas()
 
-controller_supervisores.reconciliar_pessoas()
+    while True:
+            opcoes = ["Menu de Atividades", "Menu de Pessoas", "Menu de Supervisores", "Gerar Relação Gmax" ,"Salvar e Sair", "Sair sem Salvar"]
+            opcao_selecionada = MenuSimples("Menu Principal", opcoes).choose(include_exit=False)
 
-while True:
-    try:
-        opcoes = ["Menu de Atividades", "Menu de Pessoas", "Menu de Supervisores", "Gerar Relação Gmax" ,"Salvar e Sair", "Sair sem Salvar"]
-        opcao_selecionada = MenuSimples("Menu Principal", opcoes).choose(include_exit=False)
+            match opcao_selecionada:
+                case 0:
+                    while True:
+                        opcao_menu = controller_atividades.menu_principal()
+                        match opcao_menu:
+                            case 0:
+                                print(controller_atividades.cadastrar())
+                                input()
+                            case 1:
+                                print(controller_atividades.remover())
+                                controller_pessoas.reconciliar_atividades()
+                                input()
+                            case 2:
+                                print(controller_atividades.alterar())
+                                input()
+                            case 3:
+                                controller_atividades.listar("Atividades cadastradas", choose_flag=False)
+                                input()
+                            case None:
+                                print("Retornando ao menu principal.")
+                                input()
+                                break
+                            case _:
+                                raise ValueError("Erro no atributo selecionado")
 
-        match opcao_selecionada:
-            case 0:
-                while True:
-                    opcao_menu = controller_atividades.menu_principal()
+                case 1:
+                    while True:
+                        opcao_menu = controller_pessoas.menu_principal()
+                        match opcao_menu:
+                            case 0:
+                                print(controller_pessoas.cadastrar())
+                                input()
+                            case 1:
+                                msg = controller_pessoas.remover()
+                                print(msg)
+                                controller_supervisores.reconciliar_pessoas()
+                                input()
+                            case 2:
+                                print(controller_pessoas.alterar())
+                                input()
+                            case 3:
+                                controller_pessoas.listar("Pessoas cadastradas", choose_flag=False)
+                                input()
+                            case None:
+                                print("Retornando ao menu principal.")
+                                input()
+                                break
+                            case _:
+                                raise ValueError("Erro no atributo selecionado")
+
+                case 2:
+                    opcao_menu = controller_supervisores.menu_principal()
                     match opcao_menu:
                         case 0:
-                            print(controller_atividades.cadastrar())
+                            print(controller_supervisores.cadastrar())
                             input()
                         case 1:
-                            print(controller_atividades.remover())
-                            controller_pessoas.reconciliar_atividades()
+                            print(controller_supervisores.remover())
                             input()
                         case 2:
-                            print(controller_atividades.alterar())
+                            print(controller_supervisores.alterar())
                             input()
                         case 3:
-                            controller_atividades.listar("Atividades cadastradas", choose_flag=False)
+                            controller_supervisores.listar("Pessoas cadastradas", choose_flag=False)
                             input()
                         case None:
                             print("Retornando ao menu principal.")
@@ -52,85 +97,38 @@ while True:
                             break
                         case _:
                             raise ValueError("Erro no atributo selecionado")
+                case 3:
+                    start_date = datetime.strptime(input("Entre com a data inicial (dd/mm/aaaa): "), "%d/%m/%Y")
+                    end_date = datetime.strptime(input("Entre com a data final (dd/mm/aaaa): "), "%d/%m/%Y")
 
-            case 1:
-                while True:
-                    opcao_menu = controller_pessoas.menu_principal()
-                    match opcao_menu:
-                        case 0:
-                            print(controller_pessoas.cadastrar())
-                            input()
-                        case 1:
-                            msg = controller_pessoas.remover()
-                            print(msg)
-                            controller_supervisores.reconciliar_pessoas()
-                            input()
-                        case 2:
-                            print(controller_pessoas.alterar())
-                            input()
-                        case 3:
-                            controller_pessoas.listar("Pessoas cadastradas", choose_flag=False)
-                            input()
-                        case None:
-                            print("Retornando ao menu principal.")
-                            input()
-                            break
-                        case _:
-                            raise ValueError("Erro no atributo selecionado")
+                    if start_date > end_date:
+                        raise ValueError("Data de início deve ser anterior à data final.")
 
-            case 2:
-                opcao_menu = controller_supervisores.menu_principal()
-                match opcao_menu:
-                    case 0:
-                        print(controller_supervisores.cadastrar())
-                        input()
-                    case 1:
-                        print(controller_supervisores.remover())
-                        input()
-                    case 2:
-                        print(controller_supervisores.alterar())
-                        input()
-                    case 3:
-                        controller_supervisores.listar("Pessoas cadastradas", choose_flag=False)
-                        input()
-                    case None:
-                        print("Retornando ao menu principal.")
-                        input()
-                        break
-                    case _:
-                        raise ValueError("Erro no atributo selecionado")
-            case 3:
-                start_date = datetime.strptime(input("Entre com a data inicial (dd/mm/aaaa): "), "%d/%m/%Y")
-                end_date = datetime.strptime(input("Entre com a data final (dd/mm/aaaa): "), "%d/%m/%Y")
+                    controller_acoes_conc_gmax = ControllerAcoesConcGmax(controller_pessoas.lista_pessoas, start_date, end_date)
+                    print(controller_acoes_conc_gmax.gerar_producao_por_supervisor(controller_supervisores.lista_supervisores))
+                case 4:
+                    retorno_salvar_atividades = controller_atividades.salvar()
+                    retorno_salvar_pessoas = controller_pessoas.salvar()
+                    retorno_salvar_supervisores = controller_supervisores.salvar()
 
-                if start_date > end_date:
-                    raise ValueError("Data de início deve ser anterior à data final.")
+                    print(retorno_salvar_atividades)
+                    print(retorno_salvar_pessoas)
+                    print(retorno_salvar_supervisores)
 
-                controller_acoes_conc_gmax = ControllerAcoesConcGmax(controller_pessoas.lista_pessoas, start_date, end_date)
-                print(controller_acoes_conc_gmax.gerar_producao_por_supervisor(controller_supervisores.lista_supervisores))
-            case 4:
-                retorno_salvar_atividades = controller_atividades.salvar()
-                retorno_salvar_pessoas = controller_pessoas.salvar()
-                retorno_salvar_supervisores = controller_supervisores.salvar()
+                    if (retorno_salvar_atividades == "Arquivo de atividades salvo com sucesso." and retorno_salvar_pessoas == "Arquivo de pessoas salvo sucesso."
+                            and retorno_salvar_supervisores == "Arquivo de supervisores salvo sucesso."):
+                        input("\nObrigado por utilizar o programa.\nAutor: Caio Cezar Dias\nContato: caiocd007@gmail.com\n\nPressione ENTER para fechar.")
+                        sys.exit()
 
-                print(retorno_salvar_atividades)
-                print(retorno_salvar_pessoas)
-                print(retorno_salvar_supervisores)
+                    print("Não foi possível salvar e sair.")
 
-                if (retorno_salvar_atividades == "Arquivo de atividades salvo com sucesso." and retorno_salvar_pessoas == "Arquivo de pessoas salvo sucesso."
-                        and retorno_salvar_supervisores == "Arquivo de supervisores salvo sucesso."):
+                case 5:
                     input("\nObrigado por utilizar o programa.\nAutor: Caio Cezar Dias\nContato: caiocd007@gmail.com\n\nPressione ENTER para fechar.")
                     sys.exit()
+                case _:
+                    input("A opções selecionada foi inválida")
 
-                print("Não foi possível salvar e sair.")
-
-            case 5:
-                input("\nObrigado por utilizar o programa.\nAutor: Caio Cezar Dias\nContato: caiocd007@gmail.com\n\nPressione ENTER para fechar.")
-                sys.exit()
-            case _:
-                input("A opções selecionada foi inválida")
-
-    except Exception as e:
-        print(f"Erro na iteração principal. {str(e)}")
-        traceback.print_exc()
-        input()
+except Exception as e:
+    print(f"Erro na iteração principal. {str(e)}")
+    traceback.print_exc()
+    input()
